17ce96143f2eed90b1004b06608309bb3fec68fe
abdulkaderit <abdulkaderit95@gmail.com>
2025-01-23 20:35:10 +0600

    class9 MyApp

---
+++ lib/MyApp.dart
@@ -0,0 +1,30 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'HomeActivety.dart';
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          primaryColor: Colors.pink,
+          appBarTheme: AppBarTheme(
+              color: Colors.tealAccent,
+              centerTitle: true
+          ),
+          scaffoldBackgroundColor: Colors.white60,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.pink,
+                  foregroundColor: Colors.white
+              ))
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeActivety(),
+    );
+  }
+}
