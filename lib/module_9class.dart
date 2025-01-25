567451ae005a021e3cd9f9334c3086976cb58da6
abdulkaderit <abdulkaderit95@gmail.com>
2025-01-25 19:12:54 +0600

    MediaQuery part

--- lib/module_9class.dart
+++ lib/module_9class.dart
@@ -43,3 +43,104 @@ class _module9class1State extends State<module9class1> {
     );
   }
 }
+
+class WrapExample extends StatelessWidget{
+
+  final List<String> categories = [
+    'Fruits',"Vegetable","Dairy", "Meat","Fish","Bakery","snacks","Drink"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+       title: Text('Wrap Example'),
+     ),
+     body: Column(
+       children: [
+         Padding(
+           padding: const EdgeInsets.all(8.0),
+           child: Wrap(
+             spacing: 10,
+             runSpacing: 20,
+             children: categories.map((category){
+               return Chip(label: Text(category),
+               backgroundColor: Colors.blueAccent,);
+             }).toList()
+               // Container(
+               //   height: 50,
+               //   width: 50,
+               //   color: Colors.red,
+               // ),
+               // SizedBox(width: 10,),
+               // Container(
+               //   height: 50,
+               //   width: 50,
+               //   color: Colors.red,
+               // ),
+               // SizedBox(width: 10,),
+               // Container(
+               //   height: 50,
+               //   width: 50,
+               //   color: Colors.red,
+               // ),
+               // SizedBox(width: 10,),
+               // Container(
+               //   height: 50,
+               //   width: 50,
+               //   color: Colors.red,
+               // ),
+               // SizedBox(width: 10,),
+               // Container(
+               //   height: 50,
+               //   width: 50,
+               //   color: Colors.red,
+               // ),
+               // SizedBox(width: 10,),
+               // Container(
+               //   height: 50,
+               //   width: 50,
+               //   color: Colors.red,
+               // ),
+               // SizedBox(width: 10,),
+               // Container(
+               //   height: 50,
+               //   width: 50,
+               //   color: Colors.red,
+               // ),
+               // SizedBox(width: 10,),
+               // Container(
+               //   height: 50,
+               //   width: 50,
+               //   color: Colors.red,
+               // ),
+               // SizedBox(width: 10,),
+               // Container(
+               //   height: 50,
+               //   width: 50,
+               //   color: Colors.red,
+               // ),
+               // SizedBox(width: 10,),
+               // ListTile(
+               //   title: Text("Data testing"),
+               // ),
+               // ListTile(
+               //   title: Text("Data testing"),
+               // ),
+               // ListTile(
+               //   title: Text("Data testing"),
+               // ),
+           ),
+         ),
+         GridView.count(
+             crossAxisCount: 3,
+             crossAxisSpacing:10,
+           mainAxisSpacing: 20,
+           children:List.generate(10, (index)=> Chip(label: Text("Item ${index}"),backgroundColor: Colors.blueAccent,)),
+         )
+       ],
+     ),
+   );
+  }
+
+}
